dc7862d816c2cd04eb676b330e244166a6b18f97
sahilkarnekar1 <sahilkarnekar1@gmail.com>
2024-11-18 19:07:13 +0530

    update application form

--- src/Applicant/ApplicationForm1.jsx
+++ src/Applicant/ApplicationForm1.jsx
@@ -28,6 +28,17 @@ const [formData, setFormData] = useState({
   education: '',
   jobDesignation:'',
   offer:'',
+  preferredLocation:'',
+  noticePeriod:'',
+  totalExperience:'',
+  relevantExperience:'',
+  availabilityForInterview:'',
+  expectedJoiningDate:'',
+  extraCertification:'',
+  additionalInformation1:'',
+  additionalInformation2:'',
+  additionalInformation3:'',
+
 });
  // Handle input changes
  const handleInputChange = (e) => {
@@ -245,8 +256,8 @@ const handleApplyNowClick = () => {
         <p className="p11">Applicant's Preferred Location *</p>
         <div className="flextdiv">
           <input className="input1class" type="text"  placeholder="Enter Preferred Location" 
-          name="education"
-          value={formData.education}
+          name="preferredLocation"
+          value={formData.preferredLocation}
           onChange={handleInputChange}
           />
           <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-301q99-80 149.5-154T680-594q0-90-56-148t-144-58q-88 0-144 58t-56 148q0 65 50.5 139T480-301Zm0 101Q339-304 269.5-402T200-594q0-125 78-205.5T480-880q124 0 202 80.5T760-594q0 94-69.5 192T480-200Zm0-320q33 0 56.5-23.5T560-600q0-33-23.5-56.5T480-680q-33 0-56.5 23.5T400-600q0 33 23.5 56.5T480-520ZM200-80v-80h560v80H200Zm280-520Z" /></svg>
@@ -256,7 +267,11 @@ const handleApplyNowClick = () => {
       <div className="div11">
         <p className="p11">Applicant's Notice Period *</p>
         <div className="flextdiv">
-          <input className="input1class" type="text"  placeholder="Enter Notice Period" />
+          <input className="input1class" type="text"  placeholder="Enter Notice Period" 
+          name="noticePeriod"
+          value={formData.noticePeriod}
+          onChange={handleInputChange}
+          />
           <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg>
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
@@ -266,7 +281,11 @@ const handleApplyNowClick = () => {
       <div className="div11">
         <p className="p11">Total Experience (Years) *</p>
         <div className="flextdiv">
-          <input className="input1class" type="text"  placeholder="Enter Total Experience" />
+          <input className="input1class" type="text"  placeholder="Enter Total Experience" 
+          name="totalExperience"
+          value={formData.totalExperience}
+          onChange={handleInputChange}
+          />
           <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-280q-17 0-29.5-12.5T438-322q0-17 12.5-29.5T480-364q17 0 29.5 12.5T522-322q0 17-12.5 29.5T480-280Zm-30-128q0-46 7.5-63t42.5-47q14-14 24-27.5t10-30.5q0-18-13.5-32T480-622q-27 0-41 15.5T420-574l-54-22q12-35 41-59.5t73-24.5q47 0 80.5 25.5T594-578q0 24-12 45t-30 39q-30 30-36 42t-6 44h-60ZM280-40q-33 0-56.5-23.5T200-120v-720q0-33 23.5-56.5T280-920h400q33 0 56.5 23.5T760-840v720q0 33-23.5 56.5T680-40H280Zm0-120v40h400v-40H280Zm0-80h400v-480H280v480Zm0-560h400v-40H280v40Zm0 0v-40 40Zm0 640v40-40Z" /></svg>
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
@@ -274,7 +293,11 @@ const handleApplyNowClick = () => {
       <div className="div11">
         <p className="p11">Relevant Experience *</p>
         <div className="flextdiv">
-          <input className="input1class" type="text"  placeholder="Enter Relevant Experience" />
+          <input className="input1class" type="text"  placeholder="Enter Relevant Experience" 
+          name="relevantExperience"
+          value={formData.relevantExperience}
+          onChange={handleInputChange}
+          />
           <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-280q-17 0-29.5-12.5T438-322q0-17 12.5-29.5T480-364q17 0 29.5 12.5T522-322q0 17-12.5 29.5T480-280Zm-30-128q0-46 7.5-63t42.5-47q14-14 24-27.5t10-30.5q0-18-13.5-32T480-622q-27 0-41 15.5T420-574l-54-22q12-35 41-59.5t73-24.5q47 0 80.5 25.5T594-578q0 24-12 45t-30 39q-30 30-36 42t-6 44h-60ZM280-40q-33 0-56.5-23.5T200-120v-720q0-33 23.5-56.5T280-920h400q33 0 56.5 23.5T760-840v720q0 33-23.5 56.5T680-40H280Zm0-120v40h400v-40H280Zm0-80h400v-480H280v480Zm0-560h400v-40H280v40Zm0 0v-40 40Zm0 640v40-40Z" /></svg>
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
@@ -284,7 +307,11 @@ const handleApplyNowClick = () => {
       <div className="div11">
         <p className="p11">Availability For Interview</p>
         <div className="flextdiv">
-          <input className="input1class" type="date"  placeholder="Availability For Interview" />
+          <input className="input1class" type="date"  placeholder="Availability For Interview" 
+          name="availabilityForInterview"
+          value={formData.availabilityForInterview}
+          onChange={handleInputChange}
+          />
           <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-800q134 0 227 93t93 227q0 134-93 227t-227 93q-134 0-227-93t-93-227q0-134 93-227t227-93Zm0 560q100 0 170-70t70-170q0-100-70-170t-170-70q-100 0-170 70t-70 170q0 100 70 170t170 70Zm0-100q48 0 86-27.5t54-72.5H340q16 45 54 72.5t86 27.5ZM340-560q0 17 11.5 28.5T380-520q17 0 28.5-11.5T420-560q0-17-11.5-28.5T380-600q-17 0-28.5 11.5T340-560Zm200 0q0 17 11.5 28.5T580-520q17 0 28.5-11.5T620-560q0-17-11.5-28.5T580-600q-17 0-28.5 11.5T540-560ZM40-720v-120q0-33 23.5-56.5T120-920h120v80H120v120H40ZM240-40H120q-33 0-56.5-23.5T40-120v-120h80v120h120v80Zm480 0v-80h120v-120h80v120q0 33-23.5 56.5T840-40H720Zm120-680v-120H720v-80h120q33 0 56.5 23.5T920-840v120h-80ZM480-480Z" /></svg>
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
@@ -292,7 +319,11 @@ const handleApplyNowClick = () => {
       <div className="div11">
         <p className="p11">Expected Joining Date</p>
         <div className="flextdiv">
-          <input className="input1class" type="date"  placeholder="Enter Contact Number" />
+          <input className="input1class" type="date"
+          name="expectedJoiningDate"
+          value={formData.expectedJoiningDate}
+          onChange={handleInputChange}
+          />
           <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M0-240v-63q0-43 44-70t116-27q13 0 25 .5t23 2.5q-14 21-21 44t-7 48v65H0Zm240 0v-65q0-32 17.5-58.5T307-410q32-20 76.5-30t96.5-10q53 0 97.5 10t76.5 30q32 20 49 46.5t17 58.5v65H240Zm540 0v-65q0-26-6.5-49T754-397q11-2 22.5-2.5t23.5-.5q72 0 116 26.5t44 70.5v63H780Zm-455-80h311q-10-20-55.5-35T480-370q-55 0-100.5 15T325-320ZM160-440q-33 0-56.5-23.5T80-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T160-440Zm640 0q-33 0-56.5-23.5T720-520q0-34 23.5-57t56.5-23q34 0 57 23t23 57q0 33-23 56.5T800-440Zm-320-40q-50 0-85-35t-35-85q0-51 35-85.5t85-34.5q51 0 85.5 34.5T600-600q0 50-34.5 85T480-480Zm0-80q17 0 28.5-11.5T520-600q0-17-11.5-28.5T480-640q-17 0-28.5 11.5T440-600q0 17 11.5 28.5T480-560Zm1 240Zm-1-280Z" /></svg>
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
@@ -305,7 +336,11 @@ const handleApplyNowClick = () => {
       <div className="div11">
         <p className="p11">Have You Done Any Courses and Certificates?</p>
         <div className="flextdiv">
-          <input className="input1class" type="text"  placeholder="Enter Your Certifications" />
+          <input className="input1class" type="text"  placeholder="Enter Your Certifications" 
+          name="extraCertification"
+          value={formData.extraCertification}
+          onChange={handleInputChange}
+          />
           <svg className="svgimg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M160-440v80h640v-80H160Zm0-440h640q33 0 56.5 23.5T880-800v440q0 33-23.5 56.5T800-280H640v200l-160-80-160 80v-200H160q-33 0-56.5-23.5T80-360v-440q0-33 23.5-56.5T160-880Zm0 320h640v-240H160v240Zm0 200v-440 440Z" /></svg>
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
@@ -315,7 +350,11 @@ const handleApplyNowClick = () => {
       <div className="div11">
         <p className="p11">What motivated you to apply for this position?</p>
         <div className="flextdiv">
-          <textarea className="textareaadditional" placeholder="Type Here..."  defaultValue={""} />
+          <textarea className="textareaadditional" placeholder="Type Here..."  
+          name="additionalInformation1"
+          value={formData.additionalInformation1}
+          onChange={handleInputChange}
+          />
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
       </div>
@@ -325,7 +364,11 @@ const handleApplyNowClick = () => {
         <p className="p11">How do you prioritize your tasks when working on multiple projects?
         </p>
         <div className="flextdiv">
-          <textarea className="textareaadditional" placeholder="Type Here..."  defaultValue={""} />
+          <textarea className="textareaadditional" placeholder="Type Here..." 
+          name="additionalInformation2"
+          value={formData.additionalInformation2}
+          onChange={handleInputChange}
+          />
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
       </div>
@@ -335,7 +378,11 @@ const handleApplyNowClick = () => {
         <p className="p11">How do you prioritize your tasks when working on multiple projects?
         </p>
         <div className="flextdiv">
-          <textarea className="textareaadditional" placeholder="Type Here..."  defaultValue={""} />
+          <textarea className="textareaadditional" placeholder="Type Here..."
+          name="additionalInformation3"
+          value={formData.additionalInformation3}
+          onChange={handleInputChange}
+          />
         </div>
         <p className="requiredError">This Field Is Required<img src="error.svg"   /></p>
       </div>
