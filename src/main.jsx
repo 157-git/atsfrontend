92b5f69f7045e817a621ff4d4368fbc5ec0c5ffc
sahilkarnekar1 <sahilkarnekar1@gmail.com>
2024-12-11 10:48:00 +0530

    notification work with socket but commented

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // this is commented by sahil karnekar because it was creating problem while user connecting with socket client because it renders the component twise
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+ // </React.StrictMode>
 )
