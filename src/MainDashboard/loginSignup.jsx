05b4adabd0913d75d25fddff9907a1b69cf5f13d
sahilkarnekar1 <sahilkarnekar1@gmail.com>
2025-01-29 13:55:04 +0530

    captcha done

--- src/MainDashboard/loginSignup.jsx
+++ src/MainDashboard/loginSignup.jsx
@@ -449,7 +449,7 @@ const handleRefreshCaptch = () =>{
                     onClick={handleRefreshCaptch}
                     ref={canvasRef} width="250" height="50" />
                       <div className="input-groups">
-                    <i className="fas fa-user"></i>
+                    
                     <input
                       type="text"
                       placeholder="Enter Captcha"
