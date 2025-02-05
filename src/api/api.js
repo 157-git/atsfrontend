310bb162b3b6589be88530b6b408a2afa5973109
ArshadAttar <arshattar36@gmail.com>
2025-02-05 10:47:38 +0530

    Removed S from API.js File

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://rg.157careers.in/api/ats/157industries";
-export const CHAT_BASE_URL = "https://rg.157careers.in";
+export const API_BASE_URL = "http://rg.157careers.in/api/ats/157industries";
+export const CHAT_BASE_URL = "http://rg.157careers.in";
 
 // export const API_BASE_URL = "http://192.168.1.42:9090/api/ats/157industries";
 // export const CHAT_BASE_URL = "http://192.168.1.42";
