fd180bb5f597cca8f5e2f1bbf5643dacb6296568
ArshadAttar <arshattar36@gmail.com>
2025-03-08 15:13:02 +0530

    Arshad changes In daily work

--- src/EmployeeDashboard/dailyWork.jsx
+++ src/EmployeeDashboard/dailyWork.jsx
@@ -2,36 +2,29 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import "../EmployeeDashboard/dailyWork.css";
-import Profile from "../photos/profileImg.webp";
 import dummyUserImg from "../photos/DummyUserImg.png";
-import logoutImg from "../photos/download.jpeg";
 import { Modal } from "react-bootstrap";
-import CallingTrackerForm from "../EmployeeSection/CallingTrackerForm";
 import { API_BASE_URL } from "../api/api";
-import watingImg from "../photos/fire-relax.gif";
 // line 12 to 19 added by sahil karnekar on date 14-01-2025
-import { setProfileImageFromRedux } from "../EmployeeDashboard/employeeSlice.jsx";
 //added by sahil karnekar and commented because it was implemented just for testing purpose but dont remove this
 import { Avatar, Badge, notification, List, Card } from "antd";
 import { BellOutlined, CloseOutlined, ClearOutlined } from "@ant-design/icons";
 import { initializeSocket } from "./socket.jsx";
-import notificationIcon from "../assets/notificationicon.png";
 import Meta from "antd/es/card/Meta.js";
 import {
   getCurrentLogTime,
   getCurrentTimeForUpdateData,
   getFormattedDateISOYMDformat,
-  getFormattedDateOnly,
   getLateMark,
 } from "../EmployeeSection/getFormattedDateTime.jsx";
 import {
   getDailyworkData,
   putDailyworkData,
 } from "../HandlerFunctions/getDailyWorkDataByIdTypeDateReusable.jsx";
-import Stopwatch from "../HandlerFunctions/StopWatch.jsx";
-import { useDispatch, useSelector } from "react-redux";
-import { setTriggerFetch } from "../sclices/triggerSlice.jsx";
+import Stopwatch from "../HandlerFunctions/stopWatch.jsx";
+import { useSelector } from "react-redux";
 import { LogOut } from "lucide-react";
+
 
 //SwapnilRokade_DailyWork_LogoutFunctionalityWorking_31/07
 function DailyWork({
@@ -1249,8 +1242,8 @@ function DailyWork({
             `${API_BASE_URL}/save-daily-work/${employeeId}/${userType}`,
             {
               // callingCount:20,
-              dailyArchived:0,
-              dailyPending:20,
+              dailyArchived: 0,
+              dailyPending: 20,
               dailyTarget: 20,
               date: `${currentDateNew}`,
               // dayPresentStatus:"Present",
@@ -1307,7 +1300,6 @@ function DailyWork({
       console.error(error);
     }
   };
-
 
   useEffect(() => {
     fetchNewWorkId();
@@ -1536,7 +1528,6 @@ function DailyWork({
                   count={messages.length}
                 >
                   <Avatar shape="square" icon={<BellOutlined />} />
-                  {/* <img src={notificationIcon} alt="" /> */}
                 </Badge>
               </div>
             </div>
@@ -1641,13 +1632,15 @@ function DailyWork({
               {/* none working close button removed date : 23-10-2024 */}
               <Modal.Header>
                 <Modal.Title className="dw-modal-title">
-                Break Time! ⏳
+                  Break Time! ⏳
                 </Modal.Title>
               </Modal.Header>
               <div>
-                <img 
-          src="https://t4.ftcdn.net/jpg/11/13/64/07/240_F_1113640772_HCjT1oIW0IN4DjKTP6FA33dsWrL1G0g4.jpg"
-                 alt="Waiting" className="dw-waiting-img" />
+                <img
+                  src="https://t4.ftcdn.net/jpg/11/13/64/07/240_F_1113640772_HCjT1oIW0IN4DjKTP6FA33dsWrL1G0g4.jpg"
+                  alt="Waiting"
+                  className="dw-waiting-img"
+                />
               </div>
               <Modal.Footer className="dw-modal-footer">
                 <div className="dw-resume-div">
@@ -1656,7 +1649,7 @@ function DailyWork({
                     className="profile-resume-button"
                     onClick={handleResume}
                   >
-                 <LogOut size={24} color="black" />
+                    <LogOut size={24} color="black" />
                   </button>
                 </div>
               </Modal.Footer>
