d3b021c8722872506da00b5874bd1ed1181f2787
157-git <157iplcamp@gmail.com>
2025-04-09 18:18:32 +0530

    Arshad 1st Pushed from Ajinky Sir Laptop On 9 April 2025

--- src/api/api.js
+++ src/api/api.js
@@ -11,6 +11,9 @@ export const CHAT_BASE_URL = "https://rg.157careers.in";
 // export const API_BASE_URL = "http://localhost:9090/api/ats/157industries";
 // export const CHAT_BASE_URL = "http://localhost";
 
+// export const API_BASE_URL = "http://localhost:9090/api/ats/157industries";
+// export const CHAT_BASE_URL = "http://localhost";
+
 export const addEmployee = (employee) => {
   return axios.post(`${API_BASE_URL}/add-payroll-details`, employee);
 };
