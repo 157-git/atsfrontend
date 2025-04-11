4da769c6e276a8675553d7945492a08b4bfc8c7c
ArshadAttar <arshattar36@gmail.com>
2025-04-11 13:21:49 +0530

    Arshad Pushed While deployment 11-4-2025

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,7 @@ export const API_BASE_URL = "https://rg.157careers.in/api/ats/157industries";
 export const CHAT_BASE_URL = "https://rg.157careers.in";
 
 
+//Use local system Ip address for testing
 // export const API_BASE_URL = "http://192.168.1.41:9090/api/ats/157industries";
 // export const CHAT_BASE_URL = "http://192.168.1.41";
 
