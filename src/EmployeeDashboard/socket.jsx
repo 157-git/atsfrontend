3f1f70894fa5a684ad236ccfa29fe3ba1807a37a
S3009 <sakshikashid1@gmail.com>
2026-01-08 18:08:10 +0530

    adding socket old file by sakshi

--- src/EmployeeDashboard/socket.jsx
+++ src/EmployeeDashboard/socket.jsx
@@ -1,3 +1,64 @@
+// import { io } from "socket.io-client";
+// import { CHAT_BASE_URL } from "../api/api";
+
+// let socket;
+
+// export const initializeSocket = (userId, userType) => {
+//   const query = { userId, role: userType };
+
+// socket = io("https://rg.157careers.in", {
+//   path: "/socket.io",
+//   transports: ["websocket"],
+//   query: {
+//     userId,
+//     role: userType
+//   }
+// });
+
+//   return socket;
+// };
+
+// export const getSocket = () => socket;
+
+//-------------------------------------------------------------------------
+
+// import { io } from "socket.io-client";
+
+// let socket = null;
+
+// export const initializeSocket = (userId, userType) => {
+//   if (!socket) {
+//     socket = io("http://localhost:9092", {
+//       transports: ["websocket"], // force stable transport
+//       query: {
+//         userId,
+//         role: userType,
+//       },
+//     });
+
+//     console.log("🧪 FRONTEND SOCKET HANDSHAKE", {
+//   userId,
+//   userType
+// });
+
+
+//     socket.on("connect", () => {
+//       console.log("✅ Socket connected:", socket.id);
+//     });
+
+//     socket.on("disconnect", (reason) => {
+//       console.log("❌ Socket disconnected:", reason);
+//     });
+//   }
+
+//   return socket;
+// };
+
+// export const getSocket = () => socket;
+
+
+//------------------------------------------------------------------------------------
+
 import { io } from "socket.io-client";
 import { CHAT_BASE_URL } from "../api/api";
 
@@ -6,13 +67,13 @@ let socket;
 export const initializeSocket = (userId, userType) => {
   const query = { userId, role: userType };
 
-  socket = io(`${CHAT_BASE_URL}:9092`, {
-    query,
-  });
+socket = io(CHAT_BASE_URL, {
+  path: "/socket.io",
+  query,
+  transports: ["websocket", "polling"],
+  withCredentials: true,
+});
   return socket;
 };
 
 export const getSocket = () => socket;
-
-// const protocol = window.location.protocol === "https:" ? "wss" : "ws";
-// socket = io(`${protocol}://rg.157careers.in:9092`, { query});
